f8516609a84fa500004ff397d1c0dace158d191f
Bryan Houlton <brhoulton@gmail.com>
2024-12-10 14:58:37 -0800

    fix(all): al

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { ArrowUpRight } from 'lucide-react';
-import { TypeAnimation } from 'react-type-animation';
+import { ArrowUpRight } from "lucide-react";
+import { TypeAnimation } from "react-type-animation";
 
 const HIRING_BUTTON = (
   <a href="mailto:brhoulton@gmail.com">
-    <button className="rounded-lg border flex gap-2 justify-center px-2 py-1 text-sm items-center bg-slate-950 text-white">
+    <button className="rounded-lg border flex gap-2 justify-center px-2 py-1 text-sm items-center">
       we're hiring
       <ArrowUpRight size={18} />
     </button>
@@ -12,12 +12,12 @@ const HIRING_BUTTON = (
 
 function App() {
   return (
-    <div className="p-16 w-fit flex flex-col gap-4">
+    <div className="p-6 md:p-10 lg:p-16 w-fit flex flex-col gap-4">
       <TypeAnimation
         sequence={["incortical"]}
         repeat={0}
         wrapper="span"
-        className="text-5xl"
+        className="text-5xl rounded-lg bg-black text-white p-2 -ml-2 w-fit"
         speed={50}
         cursor={false}
       />
@@ -25,17 +25,18 @@ function App() {
       <h3 className="text-slate-700 text-xl">building brain-inspired ai</h3>
 
       <p>
-        Intelligence<sup>[1]</sup> needs {"<"}86B neurons to work. <br></br>
+        Intelligence<sup>[1]</sup> needs {"<"}86B neurons to work.
+      </p>
+      <p>
         Event-based neuronal systems can already scale to billions of neurons.
         <sup>[2]</sup>
-        <br></br>
-        We're building the software to bridge the gap.
       </p>
+      <p>We're building the software to bridge the gap.</p>
       {HIRING_BUTTON}
 
       <div className="absolute bottom-0 right-0 left-0 flex justify-between p-4 text-sm text-slate-500">
         <p>Copyright © 2024 Incortical</p>
-        <ul className="flex gap-2 divide-x">
+        <ul className="flex gap-2 divide-x text-right">
           <li>
             <sup>[1]</sup>
             <a href="https://arxiv.org/pdf/1911.01547">
