bf6d5445b6a4ae4138e1ecbe33357de562c93a48
Bryan Houlton <brhoulton@gmail.com>
2025-02-01 14:49:15 -0800

    fix(posthog): font

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,17 @@ import "./index.css";
 
 import React from "react";
 
+import posthog from "posthog-js";
 import ReactDOM from "react-dom/client";
 import { SlateProvider } from "slate-ui";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+posthog.init("phc_GumBfH2sDPA0iuqX5XN7e0JX2sR7uPUXCmdK3KMoWb0", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
