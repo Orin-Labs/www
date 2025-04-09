17f142341c67dc78dfd11fbcde575b2e03ea09cd
Bryan Houlton <brhoulton@gmail.com>
2025-04-08 17:00:16 -0700

    fix(memo): animation

--- src/memo.tsx
+++ src/memo.tsx
@@ -3,6 +3,7 @@ import {
   useState,
 } from 'react';
 
+import { motion } from 'framer-motion';
 import Markdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 
@@ -37,36 +38,41 @@ export default function Memo() {
         />
       </div>
 
-      <div
-        className="z-10 text-left p-8 md:p-16 md:pt-8 w-fit mx-auto h-fit md:rounded-lg bg-white relative"
-        style={{
-          boxShadow:
-            "12px 12px 24px rgba(0, 0, 0, 0.2), -12px -12px 24px rgba(255, 255, 255, 0.2)",
-        }}
-      >
-        <Markdown
-          className="max-w-4xl mx-auto markdown-body"
-          rehypePlugins={[rehypeRaw]}
-          components={{
-            ol: ({ ...props }) => (
-              <ol
-                className="ml-6 my-4"
-                style={{ listStyle: "revert" }}
-                {...props}
-              />
-            ),
-            ul: ({ ...props }) => (
-              <ul
-                className="ml-6 my-4"
-                style={{ listStyle: "revert" }}
-                {...props}
-              />
-            ),
+      {content && (
+        <motion.div
+          className="z-10 text-left p-8 md:p-16 md:pt-8 w-fit mx-auto h-fit md:rounded-lg bg-white relative"
+          style={{
+            boxShadow:
+              "12px 12px 24px rgba(0, 0, 0, 0.2), -12px -12px 24px rgba(255, 255, 255, 0.2)",
           }}
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.25 }}
         >
-          {content}
-        </Markdown>
-      </div>
+          <Markdown
+            className="max-w-4xl mx-auto markdown-body"
+            rehypePlugins={[rehypeRaw]}
+            components={{
+              ol: ({ ...props }) => (
+                <ol
+                  className="ml-6 my-4"
+                  style={{ listStyle: "revert" }}
+                  {...props}
+                />
+              ),
+              ul: ({ ...props }) => (
+                <ul
+                  className="ml-6 my-4"
+                  style={{ listStyle: "revert" }}
+                  {...props}
+                />
+              ),
+            }}
+          >
+            {content}
+          </Markdown>
+        </motion.div>
+      )}
     </div>
   );
 }
