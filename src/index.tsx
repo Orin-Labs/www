9660042480d7010577c2d9c4da7d2e7c633590e0
Bryan Houlton <brhoulton@gmail.com>
2025-06-12 15:39:13 -0700

    fix(posthog): proxy

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,9 @@ import reportWebVitals from './reportWebVitals';
 
 if (window.location.hostname !== "localhost") {
   posthog.init("phc_GumBfH2sDPA0iuqX5XN7e0JX2sR7uPUXCmdK3KMoWb0", {
-    api_host: "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    api_host: "https://d3nk0c1kx1c6s3.cloudfront.net",
+    ui_host: "https://us.posthog.com",
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
   });
 }
 
@@ -36,4 +37,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 reportWebVitals();
